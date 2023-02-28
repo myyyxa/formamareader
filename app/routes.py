8a13e4f10f4f673443acf4e8d8fb9488ee71ffe0
myyyxa <myyyxa@gmail.com>
2023-03-01 01:18:14 +0200

    есть логин логаут авторизация валидация ввода при валидации

--- app/routes.py
+++ app/routes.py
@@ -1,30 +1,52 @@
-from app import app
+from app import app,db
 import telegram
-from flask import render_template, request,flash,redirect,url_for
-from app.forms import LoginForm
+from flask import render_template, request, flash, redirect, url_for
+from app.forms import LoginForm,RegistrationForm
+from flask_login import current_user, login_user, logout_user,login_required
+from app.models import User
 from config import Config
+from werkzeug.urls import url_parse
 
 bot = telegram.Bot(token=Config.BOT_TOKEN)
 
+
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'anton))'}
-    return render_template('index.html', title='Home', user=user)
+
+    return render_template("index.html", title='Home Page')
+
 
 @app.route('/message', methods=['POST'])
 def message():
     message = request.form['message']
     return render_template('index.html', message=message)
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
 
 @app.route('/{}'.format(bot.token), methods=['POST'])
 def telegram_webhook():
@@ -33,3 +55,28 @@ def telegram_webhook():
     message_text = update.message.text
     request.post('http://localhost:5000/message', data={'message': message_text})
     return 'ok'
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
